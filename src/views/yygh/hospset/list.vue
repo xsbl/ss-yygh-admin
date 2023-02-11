35cd1eaa658ea03cb3e6310f3d3c2b394b09122e
sisheng <2843643165@qq.com>
2023-02-11 22:41:34 +0800

    给表格加loading效果

--- src/views/yygh/hospset/list.vue
+++ src/views/yygh/hospset/list.vue
@@ -13,7 +13,9 @@
         <div>
             <el-button type="danger" size="mini" @click="removeRows()">批量删除</el-button>
         </div>
-        <el-table :data="list" stripe style="width: 100%" @selection-change="handleSelectionChange">
+        <el-table v-loading="loading" element-loading-text="拼命加载中" element-loading-spinner="el-icon-loading"
+            element-loading-background="rgba(255, 255, 255, 0.9)" :data="list" stripe style="width: 100%"
+            @selection-change="handleSelectionChange">
             <el-table-column type="selection" width="55" />
             <el-table-column type="index" width="50" label="序号" />
             <el-table-column prop="hosname" label="医院名称" />
@@ -59,7 +61,8 @@ export default {
             searchObj: {},
             list: [],
             total: 0,
-            selectionList: []
+            selectionList: [],
+            loading: false
         };
     },
     created() {
@@ -67,10 +70,12 @@ export default {
     },
     methods: {
         async getList(page = 1) {
+            this.loading = true
             this.current = page;
             const res = await hospset.getHospList(this.current, this.limit, this.searchObj)
             this.list = res.data.records;
             this.total = res.data.total;
+            this.loading = false
             console.log(res);
         },
         removeDataById(id) {
@@ -83,8 +88,10 @@ export default {
                 .then(() => {
                     //确定执行then方法
                     //调用接口
+                    this.loading = true
                     hospset.deleteHospById(id)
                         .then(() => {
+                            this.loading = false
                             //提示
                             this.$message({
                                 type: "success",
